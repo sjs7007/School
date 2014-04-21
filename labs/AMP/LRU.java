6b315e03e1daa6b92077adc695fd28d60d9dda03
sjs7007 <sjs7007@gmail.com>
2014-04-21 22:09:21 +0530

    LRU added.

---
+++ labs/AMP/LRU.java
@@ -0,0 +1,90 @@
+
+
+class LRU
+{
+	public static void main(String args[])
+	{
+		//int mainMemory[] = new int[16];
+		int cache[][] = new int [4][2]; //1st col stores page number, 2nd stores time
+
+		/*for(int i=0;i<mainMemory.length;i++) //assign random values to main memory
+		{
+			mainMemory[i]=(int)(Math.random()*100);
+		}*/
+
+		int mainMemory[]={1,3,4,7,2,4,6,3,1,2,1,3,5,6,4,3};
+
+		//assign first 4 values from main memory to cache
+		for(int i=0;i<4;i++)
+		{
+			cache[i][0]=mainMemory[i];
+			for(int j=0;j<=i;j++)
+			{
+				cache[j][1]++;
+			}
+		}
+		
+		for(int i=0;i<4;i++)
+		{
+			System.out.println(cache[i][0]+" "+cache[i][1]);
+		}
+		for(int i=0;i<mainMemory.length;i++)
+		{
+			System.out.println("Fetch Page "+mainMemory[i]+"....");
+			int loc = inCache(cache,mainMemory[i]);
+			if(loc==-1)
+			{
+				System.out.println("Cache miss.");
+				loc=returnLRUPos(cache);
+				System.out.println("Page "+cache[loc][0]+" replaced by Page "+mainMemory[i]+".");
+				cache[loc][0]=mainMemory[i];
+				cache[loc][1]=0;	
+			}
+			else
+			{
+				System.out.println("Cache hit.");
+				cache[loc][1]=0;
+			}
+			//increase time of all
+			increaseTime(cache);
+		}
+	}
+
+	public static int returnLRUPos(int x[][])
+	{
+		//position with max. time is LRU
+		int maxIndex=0;
+		int maxTime=x[0][1];
+		for(int i=1;i<4;i++)
+		{
+			if(maxTime<x[i][1])
+			{
+				maxTime=x[i][1];
+				maxIndex=i;
+			}
+		}
+		return maxIndex;
+	}
+
+	public static int inCache(int x[][],int find) //loc if present,else -1
+	{
+		int loc=-1;
+		for(int i=0;i<4;i++)
+		{
+			if(x[i][0]==find)
+			{
+				loc=i;
+				break;
+			}
+		}
+		return loc;
+	}
+
+	public static void increaseTime(int x[][])
+	{
+		for(int i=0;i<4;i++)
+		{
+			x[i][1]++;
+		}
+	}
+}
