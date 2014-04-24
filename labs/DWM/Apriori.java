bc3c9604f4bd3741488da17d636e1830c71e19e5
sjs7007 <sjs7007@gmail.com>
2014-04-24 17:01:30 +0530

    Apriori step 3 : large item set also calculated. next get candidate set of next size.

--- labs/DWM/Apriori.java
+++ labs/DWM/Apriori.java
@@ -4,35 +4,45 @@ class Apriori
 {
 	public static void main(String args[])
 	{
-		int nTrans=4,nItems=5;
+		int nTrans=4,nItems=5,confidenceValue=2;
 		ArrayList<TreeSet<Integer>> transList = new ArrayList<TreeSet<Integer>>();
 		transList.add(new TreeSet<Integer>(Arrays.asList(1,2,4)));
 		transList.add(new TreeSet<Integer>(Arrays.asList(2,3,5)));
 		transList.add(new TreeSet<Integer>(Arrays.asList(1,2,3,5)));
 		transList.add(new TreeSet<Integer>(Arrays.asList(2,5)));
 
-		ArrayList<TreeSet<Integer>> setSet = new ArrayList<TreeSet<Integer>>();
-		setSet.add(new TreeSet<Integer>(Arrays.asList(1)));
-		setSet.add(new TreeSet<Integer>(Arrays.asList(2)));
-		setSet.add(new TreeSet<Integer>(Arrays.asList(3)));
-		setSet.add(new TreeSet<Integer>(Arrays.asList(4)));
-		setSet.add(new TreeSet<Integer>(Arrays.asList(5)));
+		ArrayList<TreeSet<Integer>> cSet = new ArrayList<TreeSet<Integer>>(); //cSet = candidate set
+		cSet.add(new TreeSet<Integer>(Arrays.asList(1)));
+		cSet.add(new TreeSet<Integer>(Arrays.asList(2)));
+		cSet.add(new TreeSet<Integer>(Arrays.asList(3)));
+		cSet.add(new TreeSet<Integer>(Arrays.asList(4)));
+		cSet.add(new TreeSet<Integer>(Arrays.asList(5)));
 
-		//System.out.print(transList.get(0).containsAll(Arrays.asList(setSet.get(0).toArray())));
+		//System.out.print(transList.get(0).containsAll(Arrays.asList(cSet.get(0).toArray())));
 		//Use above code to check if subset
 
-		//find count of sets from transaction
-		for(int i=0;i<nItems;i++)
+		ArrayList<TreeSet<Integer>> largeSet = new ArrayList<TreeSet<Integer>>();
+
+		//find count of sets from transaction and if > confidence value, add to large set
+		for(int i=0;i<cSet.size();i++)
 		{
 			int count=0;
 			for(int j=0;j<nTrans;j++)
 			{
-				if(transList.get(j).containsAll(Arrays.asList(setSet.get(i).toArray())))
+				if(transList.get(j).containsAll(Arrays.asList(cSet.get(i).toArray())))
 				{
 					count++;
 				}
 			}
-			System.out.println(setSet.get(i)+" : "+count);
+			System.out.println(cSet.get(i)+" : "+count);
+			if(count>=confidenceValue)
+			{
+				System.out.println("Add to large set...");
+				largeSet.add(cSet.get(i));
+			}
 		}
+
+		System.out.println("Large Item Set : "+largeSet);
+
 	}
 }
