a5261137f5962dedc065736ccb37827d640dc30a
sjs7007 <sjs7007@gmail.com>
2014-04-25 19:12:46 +0530

    mathFunction all done.

--- labs/ACN/mathFunctionClient.java
+++ labs/ACN/mathFunctionClient.java
@@ -9,12 +9,21 @@ class mathFunctionClient
 {
 	public static void main(String args[]) throws IOException
 	{
+		Scanner ip = new Scanner(System.in);
 		Socket clientSocket = new Socket("localhost",1994);
 
 		DataInputStream fromServer = new DataInputStream(clientSocket.getInputStream());
 		PrintStream toServer = new PrintStream(clientSocket.getOutputStream());
 
 		String response = fromServer.readLine();
+		System.out.println(response);
+
+		System.out.print("Enter Number 1, Operation and Number 2: ");
+		toServer.println(ip.next());
+		toServer.println(ip.next());
+		toServer.println(ip.next());
+
+		response=fromServer.readLine();
 		System.out.println(response);
 	}
 }
--- labs/ACN/mathFunctionServer.java
+++ labs/ACN/mathFunctionServer.java
@@ -18,5 +18,29 @@ class mathFunctionServer
 		PrintStream toClient = new PrintStream(connectionSocket.getOutputStream());
 
 		toClient.println("Connection Established.");
+
+		double n1,n2,result=-1;
+		String op;
+		n1 = Double.parseDouble(fromClient.readLine());
+		op = fromClient.readLine();
+		n2 = Double.parseDouble(fromClient.readLine());
+
+		if(op.equals("+"))
+		{
+			result = n1 + n2;
+		}
+		else if(op.equals("-"))
+		{
+			result = n1 - n2;
+		}
+		else if(op.equals("*"))
+		{
+			result = n1 * n2;
+		}
+		else if(op.equals("/"))
+		{
+			result = n1 / n2;
+		}
+		toClient.println(n1+" "+op+" "+n2+" is : "+result);
 	}
 }
